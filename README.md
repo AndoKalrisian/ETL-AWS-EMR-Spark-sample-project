e32bda1f1853789bd9eadfe687c2a3f4cf6642a2
Ando <digilan2005@gmail.com>
2020-03-15 09:37:01 -0700

    updated etl.py, README.md and data-validation.ipynb to meet all project requirements

--- README.md
+++ README.md
@@ -1,14 +1,78 @@
 # de-nano-project-4
 
+## Purpose
+
+To create an analytics data lake for the analytics team at Sparkify so they can find insights into what songs their users are listening to.
+
+## Database schema
+
+![alt text](/star_schema.png "Star Schema")
+
+This schema was used to optimize queries about what songs are being played.  All the infomation needed for these queries can be found by querying the fact table.  For other queries, all other tables can be accessed by using only one JOIN.
+
+## ETL Pipeline
+
+In this project, I am using pySpark running on an EMR cluster with 3 instances.  The EMR cluster running Spark allows for faster transformation of the data. The pipeline used in this project is as follows:
+0. Spin up EMR cluster and run the following steps as one step of a Spark job.
+1. Extract songs JSON data stored in a S3 Bucket, store in memory in a Spark dataframe running on an EMR cluster
+2. Extract and transform, from the songs data, the songs and artists tables according to the database schema defined above.
+3. Store songs and artist tables in S3 as individual parquet files. Also keep a local cluster copy to use in a later step.
+4. Extract logs JSON data stored in a S3 Bucket, store in memory in a Spark dataframe running on an EMR cluster.
+5. Extract and transform, from the logs data, the users and time tables according to the database schema defined above.
+6. Store users and time tables in S3 as individual parquet files.
+7. Extract and transform, from the logs data and the stored songs and artists tables, the songplays table according to the database schema defined above.
+8. Shutdown EMR cluster.
+
+## How to run this ETL process on AWS
+
+1. Create an S3 bucket and add a folder for 'src', 'output', and 'logs'.
+2. Upload 'etl.py' into the 'src' folder on S3 bucket
+3. Use the AWS CLI to spin up cluster, run Spark job and then terminate when completed. Following is a sample of AWS CLI code I used:
+```
+aws emr create-cluster --name "Spark cluster with step" --release-label emr-5.29.0 --applications Name=Spark Name=Hive Name=Livy --log-uri s3://de-nano-project-4/logs --ec2-attributes KeyName=emr-from-cli --instance-type m5.xlarge --instance-count 3 --steps Type=Spark,Name="Spark job",ActionOnFailure=CONTINUE,Args=[--deploy-mode,cluster,--master,yarn,s3://de-nano-project-4/src/etl.py] --use-default-roles --auto-terminate
+```
+
+A nicer way to view this is as follows:
+```aws emr create-cluster --name "Spark cluster with step" \
+       --release-label emr-5.29.0 \
+       --applications Name=Spark \
+       --log-uri s3://de-nano-project-4/logs \
+       --ec2-attributes KeyName=emr-from-cli \
+       --instance-type m5.xlarge \
+       --instance-count 3 \
+       --steps Type=Spark,Name="Spark job",ActionOnFailure=CONTINUE,Args=[--deploy-mode,cluster,/
+                                                                          --master,yarn,s3://de-nano-project-4/src/etl.py] /
+       --use-default-roles --auto-terminate
+```
+## Testing
+
+I included a jupyter notebook (data-validation.ipynb) with simple data validation on each of the tables saved in my S3 bucket.  This should be run on EMR using their notebook feature.
+
+## Other useful notes
+
 ### How to connect to EMR Cluster via SSH
 
 1. Create and download '.pem' credentials.
 2. Change file permissions on '.pem' file:
 ```$ chmod 0400 .ssh/my_private_key.pem```
-3. Log in to cluster:
+3. Log in to cluster (example):
 ``` $ ssh -i my_private_key.pem hadoop@ec2-54-245-27-75.us-west-2.compute.amazonaws.com```
 
-Copy files to emr cluster:
-
+#### Copy files to emr cluster:
 
 scp -i maclinux-ec2.pem etl.py hadoop@ec2-54-245-27-75.us-west-2.compute.amazonaws.com:src/
+
+### Use AWS CLI to spin up cluster, run spark job and then terminate when complete:
+
+```
+
+View cluster data:
+
+```aws emr describe-cluster --cluster-id j-id-here```
+
+This was adapted from the following article:
+[Production Data Processing with Apache Spark](https://towardsdatascience.com/production-data-processing-with-apache-spark-96a58dfd3fe7)
+
+Debugging - Where is the stack trace?
+
+First I looked in the steps 'stderr' logs and found an error related to a specific application id.  Then I went into the 'container' log folder and click on the application with that id (in this case it was the only application).  Then I went in the first 'container' folder and found the stack traceback in the 'stdout' log file.
--- etl.py
+++ etl.py
@@ -1,16 +1,10 @@
-import configparser
 import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, from_unixtime
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType, LongType, DoubleType, FloatType
 
-config = configparser.ConfigParser()
-config.read('dl.cfg')
-
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
-
 def create_spark_session():
+    print("ETL.PY INFO::create_spark_session")
     spark = SparkSession \
         .builder \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
@@ -19,38 +13,61 @@ def create_spark_session():
 
 
 def process_song_data(spark, input_data, output_data):
+    print("ETL.PY INFO::process_song_data")
     # get filepath to song data file
-    song_data = "s3n://udacity-dend/song_data/A/W/I/"
+    song_data = "s3n://udacity-dend/song_data/*/*/*"
+    
+    # create schema for song_data
+    schema = StructType([
+        StructField('artist_id', StringType(), True),
+        StructField('artist_latitude', DoubleType(), True),
+        StructField('artist_location', StringType(), True),
+        StructField('artist_longitude', DoubleType(), True),
+        StructField('artist_name', StringType(), True),
+        StructField('duration', DoubleType(), True),
+        StructField('num_songs', LongType(), True),
+        StructField('song_id', StringType(), True),
+        StructField('title', StringType(), True),
+        StructField('year', LongType(), True)
+    ])
     
     # read song data file
-    songs_data=spark.read.json(song_data)
-
+    print("ETL.PY INFO::reading in song_data from S3")
+    songs_data=spark.read.json(song_data, schema)
+        
     # create spark table view for songs data
     songs_data.createOrReplaceTempView("songs_data_table")
 
-    # extract columns to create songs table (song_id, title, artist_id, year, duration)
+    print("ETL.PY INFO::creating songs_table")
     songs_table = spark.sql('''SELECT song_id, title, artist_id, year, duration
                                FROM songs_data_table''')
 
     # write songs table to parquet files
-    songs_table.write.parquet(output_data + 'songs_table.parquet')
+    print("ETL.PY INFO::writing songs_table.parquet to S3")
+    songs_table.write.mode('overwrite').parquet(output_data + 'songs_table.parquet')
 
     # write a local copy to be used for creating songplays table
+    print("ETL.PY INFO::writing songs_table.parquet locally")
     songs_table.write.parquet('songs_table.parquet')
-
+        
     # extract columns to create artists table (artist_id, name, location, lattitude, longitude)
+    print("ETL.PY INFO::creating artists_table")
     artists_table = spark.sql('''SELECT artist_id, artist_name AS name, artist_location AS location,
                                         artist_latitude AS latitude, artist_longitude AS longitude
                                  FROM songs_data_table''')
-   
+        
     # write artists table to parquet files
-    artists_table.write.parquet(output_data + 'artists_table.parquet')
+    print("ETL.PY INFO::writing artists_table.parquet to S3")
+    artists_table.write.mode('overwrite').parquet(output_data + 'artists_table.parquet')
 
     # write a local copy to be used for creating songplays table
+    print("ETL.PY INFO::writing artists_table.parquet locally")
     artists_table.write.parquet('artists_table.parquet')
 
 
 def process_log_data(spark, input_data, output_data):
+    print("ETL.PY INFO::process log_data")
+    
     # get filepath to log data file
     log_data = 's3a://udacity-dend/log_data/*/*/*.json'
 
@@ -77,12 +94,14 @@ def process_log_data(spark, input_data, output_data):
     ])
 
     # read log data file
+    print("ETL.PY INFO::reading in log_data from S3")
     log_data = spark.read.json(log_data, schema) 
 
     # create spark table view for the log data
     log_data.createOrReplaceTempView("log_table")
     
     # filter by actions for song plays
+    print("ETL.PY INFO::filtering out song plays where page=NextSong")
     log_data = spark.sql('''SELECT *
              FROM log_table
              WHERE page = "NextSong"''')
@@ -91,47 +110,48 @@ def process_log_data(spark, input_data, output_data):
     log_data.createOrReplaceTempView("log_table")
 
     # extract columns for users table (user_id, first_name, last_name, gender, level)
+    print("ETL.PY INFO::creating users_table")
     users_table = spark.sql('''SELECT DISTINCT userId, firstName, lastName, gender, level
                                FROM log_table''')
-    
+
     # write users table to parquet files
-    users_table.write.parquet(output_data + 'users_table.parquet')
-
-    # convert timestamp to seconds from milliseconds and put in column 'start_time'
-    timestamp_to_sec = udf(lambda x: x/1000, FloatType())
-    log_data = log_data.withColumn('start_time', timestamp_to_sec('ts'))
-
-    # create datetime column from unix timestamp that was converted to seconds 
-    log_data = log_data.withColumn('datetime', from_unixtime('start_time'))
+    print("ETL.PY INFO::writing users_table.parquet to S3")
+    users_table.write.mode('overwrite').parquet(output_data + 'users_table.parquet')
+    
+    # update 'ts' column from unix timestamp that was converted to seconds
+    log_data = log_data.withColumn("ts", from_unixtime(log_data.ts / 1000))
     
     # update spark table view
-    log_data.createOrReplaceTempView("log_table")
-    
-    # extract columns to create time table (start_time, hour, day, week, month, year, weekday)
-    time_table = spark.sql('''SELECT DISTINCT start_time,
-                        datetime, 
-                        date_format(datetime, 'HH') AS hour,
-                        date_format(datetime, 'dd') AS day,
-                        date_format(datetime, 'w') AS week,
-                        date_format(datetime, 'MM') AS month,
-                        date_format(datetime, 'yyyy') AS year,
-                        date_format(datetime, 'u') AS weekday 
-                FROM log_table
-            ''')
+    log_data.createOrReplaceTempView("log_time_table")
 
+    # extract columns to create time table (ts, hour, day, week, month, year, weekday)
+    print("ETL.PY INFO::creating time_table")
+    time_table = spark.sql('''SELECT DISTINCT ts,
+                                              date_format(ts, 'HH') AS hour,
+                                              date_format(ts, 'dd') AS day,
+                                              date_format(ts, 'w') AS week,
+                                              date_format(ts, 'MM') AS month,
+                                              date_format(ts, 'yyyy') AS year,
+                                              date_format(ts, 'u') AS weekday 
+                              FROM log_time_table''')
+    
     # write time table to parquet files partitioned by year and month
-    time_table.write.parquet(output_data + 'time_table.parquet')
+    print("ETL.PY INFO::writing time_table.parquet to S3")
+    time_table.write.mode('overwrite').partitionBy("year", "month").parquet(output_data + 'time_table.parquet')
 
     # read in song data to use for songplays table
+    print("ETL.PY INFO::reading local songs_table.parquet")
     song_df = spark.read.parquet("songs_table.parquet")
     song_df.createOrReplaceTempView("songs_table")
 
     # read in artist data to use for songplays table
+    print("ETL.PY INFO::reading local artists_table.parquet")
     artist_df = spark.read.parquet("artists_table.parquet")
     artist_df.createOrReplaceTempView("artists_table")
 
     # extract columns from joined song and log datasets to create songplays table 
     # (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    print("ETL.PY INFO::creating songplays_table")
     songplays_table = spark.sql('''SELECT CONCAT(e.ts, '-', e.sessionId) AS songplay_id, e.ts AS start_time, e.userId AS user_id,
                                         e.level AS level, sa.song_id AS song_id, 
                                         sa.artist_id AS artist_id, e.sessionId AS session_id, 
@@ -149,7 +169,8 @@ def process_log_data(spark, input_data, output_data):
 
 
     # write songplays table to parquet files partitioned by year and month
-    songplays_table.write.parquet(output_data + 'songplays_table.parquet')
+    print("ETL.PY INFO::writing songplays_table.parquet to S3")
+    songplays_table.write.mode('overwrite').parquet(output_data + 'songplays_table.parquet')
 
 
 def main():
